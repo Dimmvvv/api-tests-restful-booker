bbd6fd7b5d6f2d0f1826e506797ab003353b7690
Dimmvvv <107928814+Dimmvvv@users.noreply.github.com>
2026-08-10 07:32:44 +0300

    feat: add 5 more get tests for api verification

--- tests/api-booking.spec.ts
+++ tests/api-booking.spec.ts
@@ -1,4 +1,4 @@
-import { test, expect } from '@playwright/test';
+import { test, expect, request } from '@playwright/test';
 
 // 1. Описываем группу тестов
 test.describe('Проверка бронирований', () => {
@@ -23,5 +23,36 @@ test.describe('Проверка бронирований', () => {
     // Метод toHaveProperty проверяет, что такое свойство вообще есть в объекте.
     expect(body).toHaveProperty('firstname');
   });
-
+  test('Проверка типов данных', async ({ request }) => {
+    const response = await request.get('/booking/2');
+    expect(response.status()).toBe(200);
+    const body = await response.json();
+    expect(typeof body.totalprice).toBe('number');
+    expect(typeof body.firstname).toBe('string');
+  });
+  test('Фильтрация по данным', async ({request}) => {
+    const response = await request.get('/booking?checkin=2014-03-13');
+    expect(response.status()).toBe(200);
+    const body = await response.json();
+    expect(Array.isArray(body)).toBeTruthy();
+    expect(body.length).toBeGreaterThan(0);
+  });
+  test('Проверка вложенного объекта', async ({request}) => {
+    const response = await request.get('/booking/5');
+    expect(response.status()).toBe(200);
+    const body = await response.json();
+    expect(body).toHaveProperty('bookingdates');
+    expect(body.bookingdates).toHaveProperty('checkin');
+    expect(body.bookingdates).toHaveProperty('checkout');
+  });   
+  test('Негативный тест на несуществующий ID', async ({request}) => {
+    const response = await request.get('booking/99999');
+    expect(response.status()).toBe(404);
+  });
+  test('Математическая проверка стоимости', async ({request}) =>{
+const response = await request.get('booking/5');
+expect(response.status()).toBe(200);
+const body = await response.json();
+expect(body.totalprice).toBeGreaterThan(0);
+  });
 });
